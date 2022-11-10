a20eefd55ef30df822d9927efc11e62b1f57a46a
Dongsheng Zhao <zhaodongsheng@bilibili.com>
2022-11-10 22:37:25 +0800

    feat: change custom event rule to kebab-case

---
+++ .changeset/big-bananas-drive.md
@@ -0,0 +1,5 @@
+---
+"@casual-ui/eslint-config-vue": patch
+---
+
+change custom event to kebab-case
--- packages/eslint/vue/index.js
+++ packages/eslint/vue/index.js
@@ -1,5 +1,11 @@
 module.exports = {
   extends: [
     '@antfu/eslint-config-vue'
-  ]
+  ],
+  rules: {
+    "vue/custom-event-name-casing": [
+      "error",
+      "kebab-case"
+    ]
+  }
 }
